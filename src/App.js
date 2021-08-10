fe91128069d36755df2a2fdb4750fe15776f3ee8
Irene Lee <69160325+coolirene@users.noreply.github.com>
2021-08-10 17:07:23 -0400

    Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Tasks  from './components/Tasks';
 import AddTasks  from './components/AddTask';
+import About  from './components/About';
 
 const App = () => {
   const[showAddTask, setShowAddTask] = useState(false)
@@ -19,6 +23,14 @@ const App = () => {
 // Fetch Tasks
 const fetchTasks = async () => {
   const res = await fetch('http://localhost:5000/tasks')
+  const data = await res.json()
+
+  return data
+}
+
+
+const fetchTask = async (id) => {
+  const res = await fetch(`http://localhost:5000/tasks/{1d}`)
   const data = await res.json()
 
   return data
@@ -54,26 +66,59 @@ await fetch(`http://localhost:5000/tasks/${id}`, {
 }
 
 // Toggle Reminder
-const toggleReminder = (id) =>{
+const toggleReminder = async (id) =>{
+  const taskToToggle = await fetchTasks(id)
+  const updTask = {
+    ...taskToToggle, reminder: !taskToToggle.reminder
+  }
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'POST',
+    headers: {
+      'Content-type':'application/json'
+    },
+    body: JSON.stringify(updTask)
+  }
+  )
+
+  const data = await res.json()
+  
   setTasks(
     tasks.map((task) => 
-      task.id === id ? {...task, reminder: !task.reminder} : task
+      task.id === id ? {...task, reminder: data.reminder} : task
   ))
 }
 
   return (
+    <Router>
     <div className='container'>
       <Header 
         onAdd={() => setShowAddTask(!showAddTask)}
         showAdd={showAddTask} 
       />
-      {showAddTask && <AddTasks onAdd={addTask}  />}
-      {tasks.length > 0 ? (
-      <Tasks tasks={ tasks } onDelete={deleteTask} onToggle={toggleReminder} /> 
-      ) : (
-        'No Tasks to show'
-      )}
+      
+      <Route
+          path='/'
+          exact
+          render={(props) => (
+            <>
+              {showAddTask && <AddTask onAdd={addTask} />}
+              {tasks.length > 0 ? (
+                <Tasks
+                  tasks={tasks}
+                  onDelete={deleteTask}
+                  onToggle={toggleReminder}
+                />
+              ) : (
+                'No Tasks To Show'
+              )}
+           </>
+          )}
+        />
+      <Route path='/about' component={About} />
+      <Footer />
     </div>    
+    </Router>
   )
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+
+
+
+const About = () => {
+  return (
+    <div>
+      <h4>Version 1.0.0</h4>
+      <Link to='/'>Go Back</Link>
+    </div>
+  )
+}
+
+export default About
